9cf2e2695b506966d6b4afe771438b4c2f9a8147
Jason Chen <windson7@gmail.com>
2025-06-15 15:59:58 -0700

    use photo to pass props instead of spread operator

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,11 +1,11 @@
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
-  const {id, location, urls, user} = props;
+  const {id, location, urls, user} = props.photo;
 
   return (
     <>
-      <img className='photoItem' src={urls.full} />
+      <img className='photoItem' src={urls.regular} />
       <img className='profilePicture' src={user.profile} />
       <span>{user.name}</span>
       <span>{location.city}, {location.country}</span>
