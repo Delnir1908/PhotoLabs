440bef67004248df497a52bc073d12078fef012f
Jason Chen <windson7@gmail.com>
2025-06-16 22:42:52 -0700

    clean up code and replace sample data with mock data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,9 @@ import React from 'react';
 import './App.scss';
 import HomeRoute from './routes/HomeRoute';
 
-// Note: Rendering a single component to build components in isolation
 const App = () => {
-  //const photos = new Array(3).fill(null);
   return (
     <div className="App">
-      {/* <div className='photo-list'>
-        {photos.map(() => <PhotoListItem key={sampleDataForPhotoListItem.id} photo={sampleDataForPhotoListItem}/>)}
-      </div> */}
       <HomeRoute />
     </div>
   );
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,29 +1,12 @@
 import "../styles/TopicList.scss";
 import TopicListItem from './TopicListItem'
-
-const sampleDataForTopicList = [
-  {
-    id: 1,
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: 2,
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: 3,
-    slug: "topic-3",
-    title: "People",
-  },
-];
+import topics from "../mocks/topics";
 
 const TopicList = () => {
-  const topics = [...sampleDataForTopicList];
+  const topicList = [...topics];
   return (
     <div className="top-nav-bar__topic-list">
-      {topics.map((topic) => <TopicListItem key={topic.id} {...topic}/>)}
+      {topicList.map((topic) => <TopicListItem key={topic.id} {...topic}/>)}
     </div>
   );
 };
