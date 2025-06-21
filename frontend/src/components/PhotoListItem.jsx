316f12cbd8350df8b02168f682b67cb6935f1e4a
Jason Chen <windson7@gmail.com>
2025-06-21 16:43:10 -0700

    add handle click event

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,9 +4,14 @@ import PhotoFavButton from './PhotoFavButton';
 const PhotoListItem = (props) => {
   const {id, location, urls, user} = props.photo;
   const {toggleFav, favList, displayAlert, setDisplayModal} = props;
+  const handleClick = (e) => {
+    e.preventDefault();
+    setDisplayModal(true);
+    console.log(e);
+  }
 
   return (
-    <div className="photo-list__item" onClick={() => setDisplayModal(true)}>
+    <div className="photo-list__item" onClick={handleClick}>
       <PhotoFavButton displayAlert={displayAlert} toggleFav={toggleFav} favList={favList} id={id}/>
       <img className='photo-list__image' src={urls.regular} />
       <div className="photo-list__user-details">
